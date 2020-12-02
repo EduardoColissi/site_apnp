afdd755352225d35b6cfb9f3206a222c973b3d9a
Leonardo-Luz <Leonardoluz07@gmail.com>
2020-12-02 16:57:42 -0300

    feat: added information in page commit

--- site_apnp/src/App.tsx
+++ site_apnp/src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Row, Col, Image} from 'antd';
+import imagem from './Images/goncalves-crespo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <Col>
+        <Row>
+          <h1>A Importância da Literatura na Comunicação</h1>
+        </Row>
+
+        <Row>
+          <h2>Parnasianismo e Simbolismo</h2>
+        </Row>
+      </Col>
+    </header>
+    
+      <body>
+        <div id='image_1'>
+        <h1>Biografia do Autor Parnasianista:</h1>
+        <h2>Antônio Gonçalves Crespo</h2>
+        <img src={imagem} />
+        </div>
+
+        <div id="audio">
+          <Col>
+            <Row>
+              <h1>Ouça também:</h1>
+            </Row>
+            <Row>
+              <h2>Podcast sobre o autor Simbolista ...</h2>
+            </Row>
+            <Row>
+              audio aqui
+            </Row>
+          </Col>
+        </div>
+      </body>
+
+  <footer>
+
+  </footer>
+
     </div>
   );
 }
